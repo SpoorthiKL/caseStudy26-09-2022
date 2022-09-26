d4a70ca122611b4b4829feec5589b8e9190b953c
SpoorthiKL <70743637+SpoorthiKL@users.noreply.github.com>
2022-09-26 16:25:40 +0530

    Add files via upload

---
+++ MarksException.java
@@ -0,0 +1,9 @@
+package com.gl.caseStudy;
+
+public class MarksException extends RuntimeException {
+      static final long serialVersionUID=1L;
+    
+    public MarksException(String message) {
+        super(message);
+}
+}
